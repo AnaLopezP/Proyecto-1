5d490be6109d299764c776ced67082bc8a477d4d
Ana López <anamuyamiga@gmail.com>
2023-10-03 19:41:38 +0200

    detalles de la pizzeria

---
+++ ejercicio 2/PizzeriaBasile.py
@@ -0,0 +1,9 @@
+#aquí está la pizzeria :) 
+#Bienvenidos a la pizzeria Basile, tenemos lo mejor de lo mejor:
+#1. Pizza de dientes 
+#2. Pizza de ojos
+#3. Pizza de pelo
+#4. Pizza de uñas
+#5. Pizza de dedos
+#Las bebidas no vienen incluidas en el menu. Un vaso de agua del grifo es 400€. Para cualquier otra bebida, tendrá que pagar con su alma.
+#Precio de amigo.
