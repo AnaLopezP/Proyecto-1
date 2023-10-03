5154389583b671f202ce8b124181a3d092c3415b
Ana López <anamuyamiga@gmail.com>
2023-10-03 19:35:20 +0200

    si señor

--- ejercicio 2/DataBaseManager.py
+++ ejercicio 2/DataBaseManager.py
@@ -1,4 +1,9 @@
 #gestiona los clientes y los pedidos (controlador) 
 #hay que trabajar con hilos.
 #clientes y pedidios aleatorios, se guardan en un fichero csv, que es la base de datos
+from Autenticador import Autenticador
+from  Usuario import Usuario
+import csv
+import config
+
 class DataBaseManager:
