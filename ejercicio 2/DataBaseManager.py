8bdd9635485bd7f1459134300700c69a9b58a0fc
Ana López <anamuyamiga@gmail.com>
2023-10-03 19:32:42 +0200

    its coffin season

---
+++ ejercicio 2/DataBaseManager.py
@@ -0,0 +1,4 @@
+#gestiona los clientes y los pedidos (controlador) 
+#hay que trabajar con hilos.
+#clientes y pedidios aleatorios, se guardan en un fichero csv, que es la base de datos
+class DataBaseManager:
