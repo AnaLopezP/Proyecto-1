5bbd0ecc4613e497da9ad7a205d0c0f67126d56f
Ana López <anamuyamiga@gmail.com>
2023-10-03 19:25:41 +0200

    muchas cositas

---
+++ ejercicio 2/Pedido.py
@@ -0,0 +1,8 @@
+class Pedido:
+    def __init__(self, n_pedido, usuario, pizza, bebida, postre, precio):
+        self.n_pedido = n_pedido
+        self.usuario = usuario
+        self.pizza = pizza
+        self.bebida = bebida
+        self.postre = postre
+        self.precio = precio
